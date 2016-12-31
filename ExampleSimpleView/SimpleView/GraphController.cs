5e7a4653cbf0bb9d03de32d87447e76519b0efcf
Jonas Jermann <jjermann2@gmail.com>
2016-12-31 13:01:00 +0100

    Add a very simple example view

---
+++ ExampleSimpleView/SimpleView/GraphController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using SharpGraph.GraphViewModel;
+
+namespace ExampleSimpleView {
+    public sealed class GraphController {
+        public GraphController() {
+            if (Environment.GetCommandLineArgs().Length <= 1) {
+                throw new ArgumentException("No filename argument given!");
+            }
+            var filename = Environment.GetCommandLineArgs()[1];
+            var graph = SharpGraph.GraphParser.GraphParser.GetGraph(new FileInfo(filename));
+            var layoutGraph = SharpGraph.GraphParser.GraphParser.GetGraphLayout(graph.ToDot());
+            Graph = new WpfGraph(layoutGraph);
+        }
+
+        public WpfGraph Graph { get; }
+    }
+}
